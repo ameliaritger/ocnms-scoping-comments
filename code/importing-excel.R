a001867b5dfbd90eb9fb44c94fb5e1bd064a80a6
ameliaritger <47021807+ameliaritger@users.noreply.github.com>
2024-08-21 11:21:26 -0700

    mass import .xlsx files

---
+++ code/importing-excel.R
@@ -0,0 +1,10 @@
+pathname <- "./data/" #name the file pathway (i.e., where in the directory the files are stored)
+file_names <- list.files(path = pathname, 
+                         full.names = F,
+                         pattern='*.xlsx') #get a list of the names of the files (only .xlsx)
+
+#take the names of the files stored in file_names, and paste them iteratively over each file as it is read in (via read_xlsx). Note: file.temp is a temporary object that "floats" for each iterative process 
+for (temp_name in file_names){
+  file.temp <- read_xlsx(paste0(pathname, temp_name))
+  assign(gsub(".xlsx", "", temp_name, fixed = T), file.temp)
+}
